c153337755f7f6d78c497b903058ae998f2f5317
tk <1048044134@qq.com>
2022-04-13 23:55:14 +0800

    二刷的题目更新

--- huawei.py
+++ huawei.py
@@ -1,73 +1,52 @@
 """
 -*- coding: utf-8 -*-
 @File  : huawei.py
-@Time  : 2022/3/30
+@Time  : 2022/4/12
 @Author: Tk <TK@bupt.edu.cn>
 @Software: PyCharm
 
-输入：板卡芯片数量 M，1~32
-     用户业务配置数量 N，1~128
-     用户业务配置与业务配置间用空格分隔
-输出：芯片编号
-     芯片资源ID
+输入：
+
+输出：
 
 """
+from collections import deque
+# a.sort(key=lambda x:(x[1], x[2])) # 按照二维列表的第一列排序，如果相同按照第二列排序
+# [i[0] for i in ans] # 获得ans这个二维数组的第0列数值并保存到列表中
+# a.insert(i, obj) # 在第i个位置插入目标obj
 
 
-M = int(input())
-N = 6
-server = input().split(" ")
+def func():
+    n = int(input())
+    s = []
+    for i in range(n):
+        s.append(list(map(int, input().split(","))))
+    strange = list(map(int, input().split(" ")))
+    result = shuchu(s, strange)
+    print(" ".join(str(i) for i in result))
 
-# M = 3
-# N = 6
-# server = ['A','B','A','B','A','A']
-# server = ['A','A','A','A','A','A']
 
-
-class Solution:
-    def ro(self, m, n, servers):
-
-        m_id = 0
-        flag = 0
-        first = 0
-        server_id = 0
-        if "A" in server:  # 判断A是否在列表里
-            m_id = 1
-            flag = 1
-        for val in server:  # 遍历列表
-            if val == 'A':  # 业务A
-                if first == 0:
-                    flag = m_id
-                    first += 1
-                if server_id >= 4:
-                    flag = m_id + 1
-                    m_id += 1
-                    server_id = 0
-                server_id += 1
-            else:           # 业务B
-                m_id += 1
-
-        if m_id > m:  # 芯片数量不够了
-            m_id = 0
-            server_id = 0
-        else:
-            if server[-1] == "A":
-                m_id = flag
+def shuchu(s, strange):
+    ans = []
+    for val in s:
+        if val[1] >= strange[2] and val[2] >= strange[3] and \
+                (strange[4] == 9 or strange[4] == val[3]) and (strange[5] == 2 or strange[5] == val[4]):
+            # cpu优先
+            if strange[1] == 1:
+                ans.append(val)
+                ans.sort(key=lambda x: (x[1], x[2], x[0]))
+                if len(ans) > strange[0]:
+                    ans.pop()
             else:
-                server_id = 4
-
-        return m_id, server_id
-
-#
-# s = Solution()
-# num, server_id = s.ro(M, N, server)
-# print(num, server_id)
-
-
+                ans.append(val)
+                ans.sort(key=lambda x: (x[2], x[1], x[0]))
+                if len(ans) > strange[0]:
+                    ans.pop()
+    ans.sort(key=lambda x: x[0])
+    m = len(ans)
+    result = [m] + [i[0] for i in ans]
+    return result
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    func()
